9833e844251300128b35a599c7ec3d02bd1b5d94
abelzer09 <aaronrbelzer@gmail.com>
2022-09-26 14:40:53 -0500

    added about.css file to css folder, added classes for future styling to about.css

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function About() {
   return (
     <div className='about_container'>
-      <h1>About Me</h1>
-      <p>Driven aspiring developer looking to utilize and build on skills learned from completing recent coding bootcamp through the University of Minnesota and previous work experience to create high quality applications and websites. Trained in HTML, JavaScript, CSS, React, Node JS, NoSql and SQL. Started in an entry level position 15 years ago and worked my way up to Department Manager. As the Department Manager I increased sales by five fold and received multiple awards for department performance during the seven years in that role. Seeking to maintain a full-time position that challenges me professionally and allows for utilization of excellent team work, time management and interpersonal skills, and problem-solving skills.</p>
+      <h1 className='about_head'>About Me</h1>
+      <p className='about_para'>Driven aspiring developer looking to utilize and build on skills learned from completing recent coding bootcamp through the University of Minnesota and previous work experience to create high quality applications and websites. Trained in HTML, JavaScript, CSS, React, Node JS, NoSql and SQL. Started in an entry level position 15 years ago and worked my way up to Department Manager. As the Department Manager I increased sales by five fold and received multiple awards for department performance during the seven years in that role. Seeking to maintain a full-time position that challenges me professionally and allows for utilization of excellent team work, time management and interpersonal skills, and problem-solving skills.</p>
       
     </div>
   )
