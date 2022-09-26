da14a7821edb9c45da68121c1714d0ab5e345823
abelzer09 <aaronrbelzer@gmail.com>
2022-09-26 15:08:19 -0500

    started building out projects.js file, built array for projects, started building out jsx for displaying projects

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import about
 
 export default function About() {
   return (
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,39 @@
 import React from 'react'
 
 export default function Projects() {
+  const projects = [
+      {
+        name: '',
+        link: '',
+        screenshot: '',
+        gitrepo: ''
+      },
+      {
+        name: '',
+        link: '',
+        screenshot: '',
+        gitrepo: ''
+      },
+      {
+        name: '',
+        link: '',
+        screenshot: '',
+        gitrepo: ''
+      },
+      {
+        name: '',
+        link: '',
+        screenshot: '',
+        gitrepo: ''
+      },
+  ]
+
   return (
-    <div>
+    <div className='projects'>
+      <div className='projects_header'>
+        <h1>Projects</h1>
+      </div>
+      <div className="projects_section"></div>
       
     </div>
   )
